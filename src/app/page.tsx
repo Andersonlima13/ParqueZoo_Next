19c05dede529ffa5bc3d3daf966107737b436d4e
unknown <anderson.lima18pb@gmail.com>
2023-04-04 22:49:45 -0300

    sql-add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,21 @@ import "../Css/style.css"
 export default function Home() {
   return (
     <div>
-      <div class = "navbar">
-     
-     <ul class = "top">
-       <li class = "zoo">zoologico</li>
-       <li>Inicio</li>
-       <li>Funcionarios</li>
-       <li>Animais</li>
-     </ul>
+      <div className = "navbar">
+        <ul className = "top">
+         <li className = "zoo">zoologico</li>
+         <li>Inicio</li>
+         <li>Funcionarios</li>
+         <li>Animais</li>
+       </ul>
 
      
       </div>
 
-      <div class="Home"> 
-      <h1 class="News">PRINCIPAL SERVIÇO AQUI</h1>
-      <p class="Name"> Paragrafo destacado aqui ivamus scelerisque sit amet nulla vel porta. Fusce gravida euismod leo vitae facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent posuere</p>
-      <button class="button"> ver mais </button>
+      <div className="Home"> 
+      <h1 className="News">PRINCIPAL SERVIÇO AQUI</h1>
+      <p className="Name"> Paragrafo destacado aqui ivamus scelerisque sit amet nulla vel porta. Fusce gravida euismod leo vitae facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent posuere</p>
+      <button className="button"> ver mais </button>
      </div>
    
       
