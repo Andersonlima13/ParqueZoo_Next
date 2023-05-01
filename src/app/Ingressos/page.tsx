ee61a347a1772dcc36cf05c9b790ab88334f1f19
unknown <anderson.lima18pb@gmail.com>
2023-04-30 22:08:47 -0300

    struct

--- src/app/Ingressos/page.tsx
+++ src/app/Ingressos/page.tsx
@@ -15,19 +15,45 @@ export default function Ingressos(){
             </div>
 
             <div className= "Container">
-                <h1>
-                   Tipo de Ingresso 
-                </h1>
-            <nav lassName="Form">
+                <h1>Tipo de Ingresso </h1>
+
+
+<nav className="Form">
+
+        <div className="Options">
             <input type="radio"></input>  
+            <span> opção 1</span>
+            <input type="radio"></input> 
+             <span> opção 1</span>
+        </div>
+
+            <div className="Nome">
             <select id="select">
                 <option value="6th">NomeIncresso1</option>
                 <option value="7th">NomeIncresso2</option>
             </select>
+            </div>
+
+             <div className="Price">
             <select id="select">
-                <option value="6th">Preço1</option>
-                <option value="7th">Preço2</option>
+                <option value="6th">NomeIncresso1</option>
+                <option value="7th">NomeIncresso2</option>
             </select>
+            </div>
+        <div className="Permission">
+        <ul className="Permission-area">
+            <li>Area permitida</li>
+            <li>Area permitida</li>
+            <li>Area permitida</li>
+            <li>Area permitida</li>
+        </ul>
+    </div>
+
+    <div className="Cor-area">
+        <h1>AREA DE COR AZUL!</h1>
+    </div>
+
+    <button className="botao">Comprar!</button>
             
 
           
